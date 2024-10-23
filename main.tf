c74d02ad41c18133f377a3ce06e44738683fb791
G S S KARTHEEK <kartheek@sails.solutions>
2024-10-24 01:10:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resoure_group_name
-  location = var.location
+# resource "azurerm_resource_group" "rg" {
+#   name     = var.resoure_group_name
+#   location = var.location
 
-  tags = {
-    environment = var.tags
-  }
-}
+#   tags = {
+#     environment = var.tags
+#   }
+# }
